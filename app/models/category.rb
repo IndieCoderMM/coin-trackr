a8ad167bc3cbcf335a4dfc3b7bec69f7c58b60c6
heinthant <hthant00chk@gmail.com>
2023-05-01 18:38:36 +0630

    Define associations and validations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,6 @@
 class Category < ApplicationRecord
   belongs_to :user
+  has_and_belongs_to_many :expenses, dependent: :destroy 
+
+  validates :name, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# Create some users
+user1 = User.create!(name: "Alice", email: "alice@example.com", password: "password")
+user2 = User.create!(name: "Bob", email: "bob@example.com", password: "password")
+
+# Create some categories for user1
+category1 = Category.create!(user: user1, name: "Food", icon: "🍔")
+category2 = Category.create!(user: user1, name: "Transportation", icon: "🚕")
+
+# Create some categories for user2
+category3 = Category.create!(user: user2, name: "Entertainment", icon: "🎉")
+
+# Create some expenses for user1
+expense1 = Expense.create!(author: user1, name: "Lunch", amount: 10, categories: [category1])
+expense2 = Expense.create!(author: user1, name: "Taxi ride", amount: 5, categories: [category2])
+expense3 = Expense.create!(author: user1, name: "Dinner", amount: 20, categories: [category1, category2])
+
+# Create some expenses for user2
+expense4 = Expense.create!(author: user2, name: "Movie ticket", amount: 15, categories: [category3])
+expense5 = Expense.create!(author: user2, name: "Concert ticket", amount: 30, categories: [category3])
